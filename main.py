803e45f4b0aa23fb2329ac5603b5331712a6bc89
btrostle <bttrostle@gmail.com>
2025-11-12 20:29:56 -0700

    project complete

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import * 
 
 def get_book_text(filepath):
@@ -7,7 +8,10 @@ def get_book_text(filepath):
 
 
 def main():
-    filepath = 'books/frankenstein.txt'
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     print(f'============ BOOKBOT ============\nAnalyzing book found at {filepath}...\n----------- Word Count ----------')
     contents = get_book_text(filepath)
     print(f'Found {count_words(contents)} total words')
